504062a7a45fe3a7cda8dce07e0aa9f9e772c0b2
kashvigarg <kashvigarg02@gmail.com>
2022-12-01 18:54:43 +0530

    initial commit

---
+++ build/CMakeFiles/cmake_starter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_starter.dir/main.cpp.obj"
+  "CMakeFiles/cmake_starter.dir/main.cpp.obj.d"
+  "cmake_starter.exe"
+  "cmake_starter.exe.manifest"
+  "cmake_starter.pdb"
+  "libcmake_starter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_starter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
